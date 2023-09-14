637a591242994985e55c9efb11b88b7d9dcc4c35
vahsimonian <simonianvah@gmail.com>
2023-09-14 23:44:50 +0700

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import ResultRow from './ResultRow'
 
 function App() {
   const [amount, setAmount] = useState('100')
-  console.log(amount)
+
   return (
     <main className='max-w-4xl mx-auto px-4 py-8'>
       <h1 className='uppercase text-6xl text-center font-semibold bg-gradient-to-br from-purple-600 to-sky-400 bg-clip-text text-transparent from-30%'>
