daecf2b328d756840ede1adb0e0cd204ed8d0840
vahsimonian <simonianvah@gmail.com>
2023-09-20 15:50:32 +0700

    commit

--- src/ResultRow.tsx
+++ src/ResultRow.tsx
@@ -1,17 +1,22 @@
 type ResultRowProps = {
   loading?: boolean
+  providerName?: string
+  btc?: string
 }
 
-function ResultRow({ loading }: ResultRowProps) {
+function ResultRow({ loading, providerName, btc }: ResultRowProps) {
   return (
-    <div className='relative border border-white/10 rounded-lg bg-gradient-to-r from-purple-500/10 to-blue-500/10 p-4 my-2 overflow-hidden'>
+    <div className='relative border min-h-[64px] border-white/10 rounded-lg bg-gradient-to-r from-purple-500/10 to-blue-500/10 p-4 my-2 overflow-hidden'>
       <div className='flex gap-4'>
-        <div>logo</div>
-        <div className='grow'>provider name</div>
-        <div className='flex gap-2'>
-          <span className='text-xl text-purple-200/80'>0.000</span>
-          <span className='text-xl text-purple-200/50'>BTC</span>
-        </div>
+        <div className='h-6'>A</div>
+        {providerName && <div>logo</div>}
+        <div className='grow'>{providerName || ''}</div>
+        {btc && (
+          <div className='flex gap-2'>
+            <span className='text-xl text-purple-200/80'>0.000</span>
+            <span className='text-xl text-purple-200/50'>BTC</span>
+          </div>
+        )}
       </div>
       {loading && (
         <div className='inset-0 absolute bg-gradient-to-r from-transparent via-blue-800/50 to-transparent skeleton-animation' />
