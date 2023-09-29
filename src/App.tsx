d818dfaa2c0bafc2e367fdaacfc800305fdb47ab
vahsimonian <simonianvah@gmail.com>
2023-09-29 10:42:42 +0700

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,76 @@
-import { useEffect, useState } from 'react'
-import AmountInput from './AmountInput'
-import ResultRow from './ResultRow'
-import axios from 'axios'
-import useDebouncedEffect from 'use-debounced-effect'
+import { useEffect, useState } from "react";
+import AmountInput from "./AmountInput";
+import ResultRow from "./ResultRow";
+import axios from "axios";
+import useDebouncedEffect from "use-debounced-effect";
 
-   
 type CachedResult = {
-  provider?: string
-  btc?: string
-  _id?: string
-}
-  
-type OfferResults = {[keys: string]:string    }
+  provider?: string;
+  btc?: string;
+  _id?: string;
+};
 
-const defaultAmount = '100'
+type OfferResults = { [keys: string]: string };
+
+const defaultAmount = "100";
 
 function App() {
-  const [amount, setAmount] = useState(defaultAmount)
-  const [prevAmount, setPrevAmount] = useState(defaultAmount)
-  const [cachedResults, setCachedResults] = useState<CachedResult[]>([])
-  const [offerResults, setOfferResults] = useState<OfferResults>({})
-  const [loading, setLoading] = useState(true)
+  const [amount, setAmount] = useState(defaultAmount);
+  const [prevAmount, setPrevAmount] = useState(defaultAmount);
+  const [cachedResults, setCachedResults] = useState<CachedResult[]>([]);
+  const [offerResults, setOfferResults] = useState<OfferResults>({});
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    axios.get('https://62tfa75d3v.us.aircode.run/cachedValues').then((res) => {
-      setCachedResults(res.data)
-      setLoading(false)
+    axios.get("https://62tfa75d3v.us.aircode.run/cachedValues").then((res) => {
+      setCachedResults(res.data);
+      setLoading(false);
+    });
+  }, []);
+
+  useDebouncedEffect(
+    () => {
+      if (amount === defaultAmount) {
+        return;
+      }
+      if (amount !== prevAmount) {
+        setLoading(true);
+        axios
+          .get(`https://62tfa75d3v.us.aircode.run/offers?amount=${amount}`)
+          .then((res) => {
+            setLoading(false);
+            setOfferResults(res.data);
+            setPrevAmount(amount);
+          });
+      }
+    },
+    300,
+    [amount]
+  );
+
+  const sortedResults: CachedResult[] = Object.keys(offerResults).map(
+    (provider) => ({
+      provider,
+      btc: offerResults[provider],
     })
-  }, [])
+  );
 
-  useDebouncedEffect(() => {
-    if(amount === defaultAmount){
-      return
-    }
-    if(amount !== prevAmount) {
-      setLoading(true)
-      axios
-      .get(`https://62tfa75d3v.us.aircode.run/offers?amount=${amount}`)
-      .then(res => {
-        setLoading(false)
-        setOfferResults(res.data)
-        setPrevAmount(amount)
-      })
-    }
-  }, 300,[amount]   )
-
-
-
-  
-  const sortedResults:CachedResult[] = Object.keys(offerResults).map((provider) => ({   
-    provider,    
-    btc:offerResults[provider],
-  }))
-
-  const showCached = amount === prevAmount
+  const showCached = amount === prevAmount;
 
   return (
-    <main className='max-w-4xl mx-auto px-4 py-8'>
-      <h1 className='uppercase text-6xl text-center font-semibold bg-gradient-to-br from-purple-600 to-sky-400 bg-clip-text text-transparent from-30%'>
+    <main className="max-w-4xl mx-auto px-4 py-8">
+      <h1 className="uppercase text-6xl text-center font-semibold bg-gradient-to-br from-purple-600 to-sky-400 bg-clip-text text-transparent from-30%">
         Find the cheapest BitCoin
       </h1>
 
-      <div className='flex justify-center mt-8'>
+      <div className="flex justify-center mt-8">
         <AmountInput
-          placeholder={'amount'}
+          placeholder={"amount"}
           value={amount}
           onChange={(e) => setAmount(e.target.value)}
         />
       </div>
-      <div className='mt-6'>
+      <div className="mt-6">
         {loading && (
           <>
             <ResultRow loading={true} />
@@ -78,23 +80,25 @@ function App() {
           </>
         )}
         {!loading &&
-          cachedResults.map((result:CachedResult) => (
+          cachedResults.map((result: CachedResult) => (
             <ResultRow
               key={result._id}
               providerName={result.provider}
               btc={result.btc}
             />
-            ))}
-        {!loading && !showCached && sortedResults.map((result:CachedResult) => (
-          <ResultRow
-            key={result._id}
-            providerName={result.provider}
-            btc={result.btc}
-          />
-        ))}
+          ))}
+        {!loading &&
+          !showCached &&
+          sortedResults.map((result: CachedResult) => (
+            <ResultRow
+              key={result._id}
+              providerName={result.provider}
+              btc={result.btc}
+            />
+          ))}
       </div>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
